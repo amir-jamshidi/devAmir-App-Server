442ff0c3006673fb05ddffcc64409a35bef1c9e3
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-01 22:11:48 +0330

    Fix : Fix Cooperation  Controller

--- Controllers/Cooperation.js
+++ Controllers/Cooperation.js
@@ -1,17 +1,15 @@
 import cooperationModel from '../Models/Cooperation.js'
 
 const create = async (req, res) => {
-
     const { fullname, email, phone, link, description } = req.body
     const cooperation = await cooperationModel.create({
         fullname, email, phone, link, description
     })
     if (cooperation) {
         res.status(200).json({ message: "Send Cooperation Success" });
-    } 
-
+    }
 }
 
 export {
-    create 
+    create
 } 
