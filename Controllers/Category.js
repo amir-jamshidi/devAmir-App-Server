4abfbce6bed8628f1fff91cb61af9cc48f183968
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-01 22:10:49 +0330

    Fix : Fix Category Controller

--- Controllers/Category.js
+++ Controllers/Category.js
@@ -18,15 +18,10 @@ const create = async (req, res) => {
     }
 
 }
-
 const remove = async (req, res) => {
-
 }
-
 const update = async (req, res) => {
-
 }
-
 const getAll = async (req, res) => {
     const categories = await categoryModel.find({}).lean();
     console.log(categories);
@@ -34,14 +29,12 @@ const getAll = async (req, res) => {
         res.status(200).json(categories);
     }
 }
-
 const getCategoryMap = async (req, res) => {
     const categories = await categoryModel.find({}).limit(4).lean();
     if (categories) {
         res.status(200).json(categories);
     }
 }
-
 const getOne = async (req, res) => {
     const { href } = req.params;
     const category = await categoryModel.findOne({ href });
