a717811439002415f898ebce92b09aa74fcb5552
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-03 12:55:10 +0330

    Fix : Fix Comments

--- Routes/Comments.js
+++ Routes/Comments.js
@@ -2,7 +2,6 @@ import express from "express";
 //--
 import AuthMiddle from '../Middlewares/Auth.js'
 import { create, getMainCommnets, gteComments } from '../Controllers/Comment.js'
-
 //--
 const router = express.Router();
 //--
