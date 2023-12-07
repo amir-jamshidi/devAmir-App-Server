7b21a7fea1e82c1588d1a798159954aa4907dd0b
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-07 12:51:31 +0330

    Fix : Fix Bug In Server File

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 import app from './app.js'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
-import questionModel from './Models/Comment.js'
 dotenv.config();
 const PORT = process.env.PORT;
 
 (async () => {
     await mongoose.connect(process.env.MONGO);
     console.log('MongoDB Connected');
-    await questionModel.deleteMany({});
 })()
 
 app.listen(PORT, () => {
