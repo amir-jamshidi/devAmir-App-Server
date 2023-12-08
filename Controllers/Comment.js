323da57a50a9cd2943c4b6495679c54f4c88c49a
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-08 13:20:42 +0330

    Fix : Fix gteMainComments

--- Controllers/Comment.js
+++ Controllers/Comment.js
@@ -20,7 +20,7 @@ const gteComments = async (req, res) => {
 
 
     const course = await courseModel.findOne({ href }).lean();
-    if (!course) { 
+    if (!course) {
         return res.status(421).json({ message: 'Course Not Found' });
     }
     const comments = await commentModel.find({ courseID: course._id, isShow: 1 }).sort({ _id: -1 }).limit(limit).populate('creatorID', 'fullname role  profile').lean();
@@ -35,7 +35,7 @@ const gteComments = async (req, res) => {
 
 }
 const getMainCommnets = async (req, res) => {
-    const comments = await commentModel.find({ score: 5 }).sort({ _id: -1 }).populate('creatorID').populate('courseID', 'href name').limit(8).lean();
+    const comments = await commentModel.find({ score: 5, isShow: 1 }).sort({ _id: -1 }).populate('creatorID').populate('courseID', 'href name').limit(8).lean();
     res.status(200).json(comments)
 }
 export {
