d9ca508725f89b11bcac4a22199701fd3de6b3fe
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-12 20:37:33 +0330

    Feat : Create Error Handling Get Meeting

--- Controllers/Session.js
+++ Controllers/Session.js
@@ -39,32 +39,38 @@ const createMeeting = async (req, res, next) => {
 
 }
 
-const getMeeting = async (req, res) => {
-    const { href } = req.params;
-    const meeting = await meetingModel.findOne({ href })
-    if (!meeting) {
-        return res.status(404).json({ message: 'Not Found' })
-    }
-    const course = await courseModel.findOne({ _id: meeting.courseID });
+const getMeeting = async (req, res , next) => {
+    try {
 
-    if (meeting && meeting.isFree === 1) {
-        return res.status(200).json(meeting);
-    }
 
-    if (req.user === null) {
-        return res.status(404).json({
-            message: "You Do Not Have Access"
+        const { href } = req.params;
+        const meeting = await meetingModel.findOne({ href })
+        if (!meeting) {
+            return res.status(404).json({ message: 'Not Found' })
+        }
+        const course = await courseModel.findOne({ _id: meeting.courseID });
+
+        if (meeting && meeting.isFree === 1) {
+            return res.status(200).json(meeting);
+        }
+
+        if (req.user === null) {
+            return res.status(404).json({
+                message: "You Do Not Have Access"
+            })
+        }
+
+        const isRegisterToCourse = await courseRegisterModel.findOne({ userID: req.user._id, courseID: course._id });
+        if (isRegisterToCourse) {
+            return res.status(200).json(meeting);
+        }
+
+        res.status(404).json({
+            message: "Not Found"
         })
+    } catch (error) {
+        next()
     }
-
-    const isRegisterToCourse = await courseRegisterModel.findOne({ userID: req.user._id, courseID: course._id });
-    if (isRegisterToCourse) {
-        return res.status(200).json(meeting);
-    }
-
-    res.status(404).json({
-        message: "Not Found"
-    })
 }
 
 
