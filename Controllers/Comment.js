d152d35a3cc3f67cb7dc0458d99ebe18009c27df
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-14 13:10:15 +0330

    Feat : Create Error Handling getComments

--- Controllers/Comment.js
+++ Controllers/Comment.js
@@ -3,7 +3,6 @@ import courseModel from '../Models/Course.js'
 import converToPersian from '../Utils/PersianDate.js';
 
 const create = async (req, res, next) => {
-
     try {
         const { body, courseID, score } = req.body;
         const comment = await commentModel.create({
@@ -16,24 +15,26 @@ const create = async (req, res, next) => {
         next();
     }
 }
-const gteComments = async (req, res) => {
+const gteComments = async (req, res, next) => {
 
-    const { href, limit } = req.params;
+    try {
+        const { href, limit } = req.params;
 
+        const course = await courseModel.findOne({ href }).lean();
+        if (!course) {
+            return res.status(421).json({ message: 'Course Not Found' });
+        }
+        const comments = await commentModel.find({ courseID: course._id, isShow: 1 }).sort({ _id: -1 }).limit(limit).populate('creatorID', 'fullname role  profile').lean();
 
-    const course = await courseModel.findOne({ href }).lean();
-    if (!course) {
-        return res.status(421).json({ message: 'Course Not Found' });
+        //Convert Date 
+        comments.forEach(comment => {
+            comment.createdAt = converToPersian(comment.createdAt);
+        })
+
+        return res.status(200).json(comments);
+    } catch (error) {
+        next();
     }
-    const comments = await commentModel.find({ courseID: course._id, isShow: 1 }).sort({ _id: -1 }).limit(limit).populate('creatorID', 'fullname role  profile').lean();
-
-    //Convert Date 
-    comments.forEach(comment => {
-        comment.createdAt = converToPersian(comment.createdAt);
-    })
-
-    return res.status(200).json(comments);
-
 
 }
 const getMainCommnets = async (req, res) => {
