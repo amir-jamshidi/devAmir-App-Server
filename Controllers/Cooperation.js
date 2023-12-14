38d0917c3908078b5b79c5d21fb7e255444f61bd
Amir Jamshidi <aabbccddee9898@gmail.com>
2023-12-14 13:08:34 +0330

    Feat : Create Error Handling create Cooperation

--- Controllers/Cooperation.js
+++ Controllers/Cooperation.js
@@ -1,15 +1,19 @@
 import cooperationModel from '../Models/Cooperation.js'
 
-const create = async (req, res) => {
-    const { fullname, email, phone, link, description } = req.body
-    const cooperation = await cooperationModel.create({
-        fullname, email, phone, link, description
-    })
-    if (cooperation) {
-        res.status(200).json({ message: "Send Cooperation Success" });
+const create = async (req, res, next) => {
+    try {
+        const { fullname, email, phone, link, description } = req.body
+        const cooperation = await cooperationModel.create({
+            fullname, email, phone, link, description
+        })
+        if (cooperation) {
+            res.status(200).json({ message: "Send Cooperation Success" });
+        }
+    } catch (error) {
+        next()
     }
 }
-  
+
 export {
     create
 } 
