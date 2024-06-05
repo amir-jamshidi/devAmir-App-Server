408ec6f1a372d6791507eac7eb337828fba96d5d
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-06-05 23:23:24 +0330

    Update App

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ import bansRoutes from './Routes/Bans.js'
 const app = express();
 //--
 import { fileURLToPath } from 'url';
-
+//-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename)
 //--
