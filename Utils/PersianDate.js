341ce5c88c0aeca7353ef8e0d3dd91a5f7b65896
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-06-18 23:52:10 +0330

    Update PersianDate

--- Utils/PersianDate.js
+++ Utils/PersianDate.js
@@ -1,5 +1,5 @@
 
-import moment from "jalali-moment"
+import moment from "jalali-moment";
 
 const converToPersian = (date) => {
     const persian = moment(date.toISOString().slice(0, 10), 'YYYY-MM-DD').locale('fa').format('YY/MM/DD');
