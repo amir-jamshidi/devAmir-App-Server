fcd79881ac3d133f2d74e9b64169c6747800dfcd
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-07-05 23:36:27 +0330

    Update MulterConfig

--- Uploads/MulterConfig.js
+++ Uploads/MulterConfig.js
@@ -6,7 +6,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 export default multer.diskStorage({
     
     destination: (req, res, cb) => {
