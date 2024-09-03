646988eafbe9899bd3271d2b1a655e0632380baa
Amir Hossein Jamshidi <48350287+amir-jamshidi@users.noreply.github.com>
2024-09-03 23:28:44 +0330

    Update PersianDate.js

--- Utils/PersianDate.js
+++ Utils/PersianDate.js
@@ -6,6 +6,5 @@ const converToPersian = (date) => {
     const persian = moment(date.toISOString().slice(0, 10), 'YYYY-MM-DD').locale('fa').format('YY/MM/DD');
     return persian;
 }
-
 export default converToPersian
 
